1d42b3f9f601648572eedc9fc62a5370793a4129
Sufyan <khan.sufyan.cs@gmail.com>
2020-10-03 09:49:14 -0400

    Added p-80 tailwind class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
-  purge: ["./src/**/*.js"],
-  theme: {},
+  purge: ["./src/**/*.jsx", "./src/**/*.js"],
+  theme: {
+    extend: {
+      padding: {
+        "80": "20rem",
+      },
+    },
+  },
   variants: {},
   // https://github.com/tailwindcss/custom-forms
   plugins: [require("@tailwindcss/custom-forms")],
