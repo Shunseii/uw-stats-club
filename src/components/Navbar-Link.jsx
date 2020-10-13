ac94d5e14b37cc1f51fe224ed8aabf5559336e03
Sufyan Khan <khan.sufyan.cs@gmail.com>
2020-10-13 16:22:30 -0400

    Adds Navbar Link component

---
+++ src/components/Navbar-Link.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "gatsby";
+
+const NavbarLink = ({ active, name, to }) => {
+  return (
+    <li>
+      <Link
+        className={`text-2xl pb-1 ${
+          active === name ? "border-b border-black" : "font-light"
+        }`}
+        to={to}
+      >
+        {name}
+      </Link>
+    </li>
+  );
+};
+
+NavbarLink.propTypes = {
+  active: PropTypes.string,
+  name: PropTypes.string,
+  to: PropTypes.string
+};
+
+export default NavbarLink;
