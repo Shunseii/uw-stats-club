380c82ddc00dcf6a71fadf4834c09f04d8e478ad
Sufyan Khan <khan.sufyan.cs@gmail.com>
2020-10-17 00:34:18 -0400

    Converts style attribute to Tailwind class

--- src/components/Navbar-Link.jsx
+++ src/components/Navbar-Link.jsx
@@ -4,10 +4,11 @@ import { Link } from "gatsby";
 
 const NavbarLink = ({ active, name, to }) => {
   return (
-    <li style={{ padding: "15px" }}>
+    <li className="px-3">
       <Link
-        className={`text-2xl pb-1 ${active === name ? "border-b border-black" : "font-light"
-          }`}
+        className={`text-2xl pb-1 ${
+          active === name ? "border-b border-black" : "font-light"
+        }`}
         to={to}
       >
         {name}
