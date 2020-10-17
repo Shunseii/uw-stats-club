88934a85c57545009c85dcec49ca7e5228529f8e
Sufyan Khan <khan.sufyan.cs@gmail.com>
2020-10-16 21:24:31 -0400

    Adds custom 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Container from "../components/Container.jsx";
+import Navbar from "../components/Navbar.jsx";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <Container>Page not found.</Container>
+    </div>
+  );
+};
+
+export default NotFoundPage;
