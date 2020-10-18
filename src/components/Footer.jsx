6ad46cbe5334b17afb8d5167b2a43184709bb0cd
Sufyan Khan <khan.sufyan.cs@gmail.com>
2020-10-17 21:26:56 -0400

    Creates footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="absolute left-0 bottom-0 right-0 text-center">
+      Copyright © 2020 University of Waterloo Statistics Club
+    </footer>
+  );
+};
+
+export default Footer;
