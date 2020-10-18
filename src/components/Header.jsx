ccace106571098083ad1f6aaa18eabe49a872e43
Sufyan Khan <khan.sufyan.cs@gmail.com>
2020-10-18 17:29:23 -0400

    Adds horizontal rule

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 
 const Header = ({ title }) => {
   return (
-    <div className="mb-8">
-      <h1 className="text-4xl text-center font-medium">{title}</h1>
-      {/* <hr className="bg-black w-4/5 mx-auto" /> */}
+    <div className="flex flex-col w-fit mx-auto mb-8">
+      <h1 className="w-fit text-4xl text-center font-medium pb-2">{title}</h1>
+      <hr className="border-black w-4/5 mx-auto" />
     </div>
   );
 };
